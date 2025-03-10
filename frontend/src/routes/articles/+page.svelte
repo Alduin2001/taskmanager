219899deb88d7325f0d327736bdc02b9fe6fef67
Alduin2001 <almas20_01@mail.ru>
2025-03-10 14:19:37 +0300

    Добавил страничку для отображения постов

---
+++ frontend/src/routes/articles/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Articles from "$lib/components/articles/Articles.svelte";
+import { Container } from "@sveltestrap/sveltestrap";
+
+
+</script>
+
+<svelte:head>
+    <title>Посты</title>
+</svelte:head>
+
+<Container>
+    Все Посты
+    <Articles />
+</Container>
+
