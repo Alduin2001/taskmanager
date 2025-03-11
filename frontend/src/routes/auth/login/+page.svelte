3024b0c379099adc76342b82afa4efc3691bb2ac
Alduin2001 <almas20_01@mail.ru>
2025-03-11 08:51:46 +0300

    Делаю рефакторинг

--- frontend/src/routes/auth/login/+page.svelte
+++ frontend/src/routes/auth/login/+page.svelte
@@ -1,23 +1,6 @@
 <script lang="ts">
-	import { Button, Col, Container, FormGroup, FormText, Input, InputGroup, InputGroupText, Row } from "@sveltestrap/sveltestrap";
-    import { goto } from "$app/navigation";
-    import { createForm } from "svelte-forms-lib";
-	import type { loginUserDto } from "$lib/interfaces/user";
-    import { loginUserSchema } from "$lib/validation/user";
-	import { loginUser } from "$lib/store/UserStore";
-    const {form,handleChange,handleSubmit,state,errors} = createForm<loginUserDto>({
-        initialValues:{
-            email:"",
-            password:""
-        },
-        validationSchema:loginUserSchema,
-        onSubmit:async (data:loginUserDto)=>{
-            const response = await loginUser(data);
-            if(response){
-                goto('/profile');
-            }
-        }
-    });
+	import { Container } from "@sveltestrap/sveltestrap";
+	import AuthUi from "$lib/components/auth/AuthUI.svelte";
 </script>
 
 <svelte:head>
@@ -25,32 +8,5 @@
 </svelte:head>
 <Container>
     <h1>Вход</h1>
-    <form on:submit={handleSubmit}>
-        <FormGroup>
-            <InputGroup>
-                <InputGroupText>@</InputGroupText>
-                <Input name="email" bind:value={$form.email} on:change={handleChange}/>
-            </InputGroup>
-            {#if $errors.email}
-                <FormText class="text-danger">*{$errors.email}</FormText>
-            {/if}
-        </FormGroup>
-        <FormGroup>
-            <InputGroup>
-                <InputGroupText>@</InputGroupText>
-                <Input type="password" name="password" bind:value={$form.password} on:change={handleChange}/>
-            </InputGroup>
-            {#if $errors.password}
-                <FormText class="text-danger">*{$errors.password}</FormText>
-            {/if}
-        </FormGroup>
-        <Row class="mt-2">
-            <Col>
-                <Button color="success" type="submit" class="w-100">Войти</Button>
-            </Col>
-            <Col>
-                <Button color="danger" class="w-100">Очистить</Button>
-            </Col>
-        </Row>
-    </form>
+    <AuthUi/>
 </Container>
